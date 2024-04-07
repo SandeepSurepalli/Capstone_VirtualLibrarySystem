1d2c88ea70b43bebc80ea08d7fd68326bfc4a143
Sandeep-2 <sandeepsurapalli25@gmail.com>
2024-04-07 14:52:51 +0530

    Commit after first push

--- src/main/java/qualitykiosk/FirstCode.java
+++ src/main/java/qualitykiosk/FirstCode.java
@@ -3,5 +3,6 @@ package qualitykiosk;
 class FirstCode{
     public static void main(String[] args){
         System.out.print("Hello World");
+        System.out.println("Checking the second push");
     }
 }
